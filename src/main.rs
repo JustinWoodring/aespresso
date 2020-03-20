cfd7e73db58fb10df3cd09af6488033a43f348eb
Booglejr <41842051+Rootyjr@users.noreply.github.com>
2020-03-20 15:57:57 -0500

    Fix stupid tabs and strange spacing.

--- src/main.rs
+++ src/main.rs
@@ -17,34 +17,34 @@ use gtk::{Application, ApplicationWindow, Button, Box, Frame, ListBox, Label};
 fn main() {
 	let app_data = app_constants::get_app_data();
 	
-    let application = Application::new(Some(app_data.gtk_path), Default::default())
-        .expect("Could not create application.");
+	let application = Application::new(Some(app_data.gtk_path), Default::default())
+		.expect("Could not create application.");
 
-    application.connect_activate(move |app| {
+	application.connect_activate(move |app| {
 		let mut app_errors = app_constants::ErrMsg::new();
 		app_errors.init();
 		let app_errors_clone = app_errors.clone();
 		let app_errors_clone2 = app_errors.clone();
 		let app_errors_clone3 = app_errors.clone();
 		
-        let window = ApplicationWindow::new(app);
-        window.set_title(app_data.title);
-        window.set_default_size(300, 500);
-		window.set_resizable(false);
-		
-		let root = Box::new(Vertical, 40);
-		let about_window = about_window::new(app, &app_data);
-	    
-        let app_quit = app.clone();
-		let app_quit2 = app.clone();
-		let app_quit3 = app.clone();
-		let app_quit4 = app.clone();
+		let window = ApplicationWindow::new(app);
+		window.set_title(app_data.title);
+		window.set_default_size(300, 500);
+			window.set_resizable(false);
+
+			let root = Box::new(Vertical, 40);
+			let about_window = about_window::new(app, &app_data);
+
+		let app_quit = app.clone();
+			let app_quit2 = app.clone();
+			let app_quit3 = app.clone();
+			let app_quit4 = app.clone();
 
 
-        window.connect_delete_event(move |_,_|{
-            app_quit.quit();
-            Inhibit(false)
-        });
+		window.connect_delete_event(move |_,_|{
+		    app_quit.quit();
+		    Inhibit(false)
+		});
 		
 		let env_list_wrapper = Box::new(Vertical,10);
 		let env_list_frame = Frame::new(Some("Java Environments"));
@@ -72,65 +72,64 @@ fn main() {
 		
 		let set_env = Button::new_with_label("Set Default");
 		set_env.set_tooltip_text(Some("Sets the selected Java environment as the default environment."));
-        set_env.connect_clicked(move |_| {	
-				match env_list_box_clone.get_selected_row() {
-					None => {}
-					Some(selected_row) => {
-						let label_text = selected_row.get_child().unwrap().downcast::<Label>().unwrap().get_label().unwrap().replace(" (default)", "");
-						let set_c = Command::new("lxqt-sudo")
-							.arg("-s")
-				            .arg("archlinux-java")
-							.arg("set")
-							.arg(label_text)
-				            .output();
-				        match set_c {
-							Ok(_text) => {}
-							Err(_text) => {util::show_message(&app_quit2, Err(app_errors_clone.get_err(1)), None);}
-						}
-						util::refresh_env(&env_list_box_clone, &list);
+		
+        	set_env.connect_clicked(move |_| {	
+			match env_list_box_clone.get_selected_row() {
+				None => {}
+				Some(selected_row) => {
+					let label_text = selected_row.get_child().unwrap().downcast::<Label>().unwrap().get_label().unwrap().replace(" (default)", "");
+					let set_c = Command::new("lxqt-sudo")
+						.arg("-s")
+				    .arg("archlinux-java")
+						.arg("set")
+						.arg(label_text)
+				    .output();
+				match set_c {
+						Ok(_text) => {}
+						Err(_text) => {util::show_message(&app_quit2, Err(app_errors_clone.get_err(1)), None);}
 					}
+					util::refresh_env(&env_list_box_clone, &list);
 				}
-        });
-        let refresh = Button::new_with_label("Refresh List");
+			}
+        	});
+        	let refresh = Button::new_with_label("Refresh List");
 		refresh.set_tooltip_text(Some("Refreshes the Java environment list."));
-        refresh.connect_clicked(move |_| {
-            //util::show_message(&app_quit4, Ok("Could not run lxqt-sudo. Please ensure that it is installed on your system."));
+		refresh.connect_clicked(move |_| {
+			//util::show_message(&app_quit4, Ok("Could not run lxqt-sudo. Please ensure that it is installed on your system."));
 			util::refresh_env(&env_list_box_clone2, &list_clone2);
-        });
-        let unset = Button::new_with_label("Unset Default Env");
+		});
+		let unset = Button::new_with_label("Unset Default Env");
 		unset.set_tooltip_text(Some("Unsets the default Java environment."));
-        unset.connect_clicked(move |_| {
-            let unset_c = Command::new("lxqt-sudo")
+		unset.connect_clicked(move |_| {
+			let unset_c = Command::new("lxqt-sudo")
 				.arg("-s")
-	            .arg("archlinux-java")
+				.arg("archlinux-java")
 				.arg("unset")
-	            .output();
-	        match unset_c {
+				.output();
+			match unset_c {
 				Ok(_text) => {}
 				Err(_text) => {util::show_message(&app_quit3, Err(app_errors_clone2.get_err(1)), None);}
 			}
 			util::refresh_env(&env_list_box_clone3, &list_clone3);
-			
-        });
-	    let fix = Button::new_with_label("Fix (Auto-select Env)");
-		fix.set_tooltip_text(Some("Tries to fix invalid Java environment links. If no default Java environment is set, it will auto-select an environment for you."));
-        fix.connect_clicked(move |_| {
-            let fix_c = Command::new("lxqt-sudo")
+        	});
+		let fix = Button::new_with_label("Fix (Auto-select Env)");
+		fix.set_tooltip_text(Some("Tries to fix invalid Java environment links. If no default Java environment is set, it will auto-select an environment for you."));		
+		fix.connect_clicked(move |_| {
+			let fix_c = Command::new("lxqt-sudo")
 				.arg("-s")
-	            .arg("archlinux-java")
+	            		.arg("archlinux-java")
 				.arg("fix")
-	            .output();
-	        match fix_c {
+	            		.output();
+			match fix_c {
 				Ok(_text) => {}
 				Err(_text) => {util::show_message(&app_quit4, Err(app_errors_clone3.get_err(1)), None);}
 			}
-			
 			util::refresh_env(&env_list_box_clone4, &list_clone4);
-        });
+        	});
 
 		let about = Button::new_with_label("About aespresso");
 		about.set_tooltip_text(Some("About this program."));
-        about.connect_clicked(move |_| {
+		about.connect_clicked(move |_| {
 			about_window.show_all();
 		});
 
@@ -155,8 +154,7 @@ fn main() {
 		root.set_border_width(10);
 		
 		window.add(&root);
-        window.show_all();
-    });
-
+        	window.show_all();
+	});
     application.run(&[]);
 }
